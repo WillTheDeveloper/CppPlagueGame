0fb13c34da8bde99dfa2944e99a298bacc50c3d9
Will <twitchifygamers@gmail.com>
2022-01-25 10:48:48 +0000

    Started to construct initial game

--- CppPlagueGame.cpp
+++ CppPlagueGame.cpp
@@ -2,5 +2,34 @@
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    class Plague;
+
+    std::string name;
+
+    std::cout << "Welcome to Plague; CPP Edition" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "." << std::endl;
+
+    class Plague {
+    public:
+        std::string plague;
+        std::string population;
+        int populationvalue;
+
+        void start() {
+            std::cout << "Please state the name of the plague:" << std::endl;
+            std::cin >> plague;
+
+            if (plague.length() > 4) {
+                population = "7.4 billion";
+                populationvalue = 7400000000;
+            }
+            else if (plague.length() < 4) {
+                population = "5.4 billion";
+                populationvalue = 5400000000;
+            }
+
+            std::cout << "The world's current population is " << population << std::endl;
+        }
+    };
 }
